a0046a399b31a67963e64ee3bf1832bf99e6f8ce
Maxim <maximus405@gmail.com>
2025-03-02 17:53:20 +0300

    separate two tests Update github workflow with dispatch

--- test/test_simple_button.py
+++ test/test_simple_button.py
@@ -7,5 +7,12 @@ def test_simple_button(browser):
     home_page.open()
     home_page.click_menu_buttons()
     buttons_page = ButtonsPage(browser)
-    buttons_page.check_buttons_page_title("Buttons")
     buttons_page.check_simple_button()
+
+
+def test_simple_button_page_title(browser):
+    home_page = HomePage(browser)
+    home_page.open()
+    home_page.click_menu_buttons()
+    buttons_page = ButtonsPage(browser)
+    buttons_page.check_buttons_page_title("Buttons")
