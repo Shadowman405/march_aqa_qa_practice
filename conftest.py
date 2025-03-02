01900e720356256089a22317fd4ecfd22967c87a
Maxim <maximus405@gmail.com>
2025-03-02 17:32:55 +0300

    update test with chrome browser

--- conftest.py
+++ conftest.py
@@ -1,13 +1,13 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture()
 def browser():
     options = Options()
     options.add_argument('--headless')
-    browser = webdriver.Firefox(options=options)
+    browser = webdriver.Chrome(options=options)
     browser.maximize_window()
     browser.implicitly_wait(3)
     yield browser
